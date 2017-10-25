e4fcd5ef8bfa2e0fca1631cd6baaa7a95b79f9b6
aektashah <aektayshah@gmail.com>
2017-10-24 23:31:28 -0400

    pwds

--- lib/microblog/accounts/user.ex
+++ lib/microblog/accounts/user.ex
@@ -9,6 +9,13 @@ defmodule Microblog.Accounts.User do
     has_many :messages, Microblog.Blog.Message
     has_many :following, Microblog.Activity.Follow, foreign_key: :following_id
     has_many :follower, Microblog.Activity.Follow
+    
+    field :password_hash, :string
+    field :pw_tries, :integer
+    field :pw_last_try, :utc_datetime
+
+    field :password, :string, virtual: true
+    field :password_confirmation, :string, virtual: true
 
     timestamps()
   end
